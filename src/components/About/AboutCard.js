958836bfa6455f7509f6f53163abd3f8e97b82a4
Nishi Kantamneni <nk475@cornell.edu>
2024-04-04 14:35:00 -0400

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Nishi Kantamneni </span>
+            from <span className="purple"> California.</span>
+            <br />
+            I am currently a computer and informations sciences major at Cornell University.
+            <br />
+            
+          </p>
+          
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Nishi</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
