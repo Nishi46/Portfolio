df3320c83f75ec1ee3dfb47dc7b68f9d2fc102ea
Nishi <46855953+Nishi46@users.noreply.github.com>
2024-05-11 17:06:56 -0400

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,11 +9,11 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  //const [width, setWidth] = useState(1200);
+  const [width, setWidth] = useState(1200);
 
-  //useEffect(() => {
-  //  setWidth(window.innerWidth);
-  //}, []);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
 
   return (
     <div>
@@ -33,7 +33,7 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1}/>
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
